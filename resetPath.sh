e7a035f3c5978606f70738181dfa4bd953f849b6
tbrownex <tom@brownex.us>
2019-06-26 23:07:11 +0000

    misc changes

---
+++ resetPath.sh
@@ -0,0 +1,3 @@
+export PATH=/home/tbrownex/.local/bin:/home/tbrownex/google-cloud-sdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/lib/jvm/java-9-openjdk-amd64/bin
+export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64
+
---
+++ setSparkPath.sh
@@ -0,0 +1,2 @@
+export PATH=/home/tbrownex/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/lib/jvm/java-8-openjdk-amd64/bin
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
